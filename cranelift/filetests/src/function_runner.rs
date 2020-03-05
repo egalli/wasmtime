73cd4594a15c9bd5067830a0f8e45865c84d3247
Andrew Brown <andrew.brown@intel.com>
2020-03-05 10:12:37 -0800

    Extend FunctionRunner with the ability to compile code and unimplemented stubs for executing this code
    This is still a WIP for several reasons:
     - it is tricky to get the right parameter types available to `execute` so that Rust can cast the compiled code to a function pointer of the correct type; likely we need to something like what Alex did in `wrappers!` and `getters!` in `crates/api/src/func.rs`.
     - FunctionRunner seems to fit better in cranelift-interpreter than cranelift-filetests; TODO we will likely move this there in the future
     - FunctionRunner is a stripped down version of SimpleJIT; it might be smart to do some more analysis of SimpleJIT to see if that should be modified to replace FunctionRunner

--- cranelift/filetests/src/function_runner.rs
+++ cranelift/filetests/src/function_runner.rs
@@ -1,10 +1,36 @@
+//! A simplistic mechanism for running functions. TODO this could be merged with SimpleJIT?
+
 use core::mem;
 use cranelift_codegen::binemit::{NullRelocSink, NullStackmapSink, NullTrapSink};
 use cranelift_codegen::ir::Function;
 use cranelift_codegen::isa::TargetIsa;
 use cranelift_codegen::{settings, Context};
 use cranelift_native::builder as host_isa_builder;
-use memmap::MmapMut;
+use cranelift_value::Value;
+use memmap::{Mmap, MmapMut}; // TODO break this cyclic dependency with cranelift-interpreter (FunctionRunner uses Value, tracing uses FunctionRunner)
+
+/// Container for compiled code to avoid exposing the underlying Mmap implementation.
+pub struct CompiledCode {
+    page: Mmap,
+}
+
+impl CompiledCode {
+    /// Return a pointer to the compiled code.
+    pub fn as_ptr(&self) -> *const u8 {
+        self.page.as_ptr()
+    }
+
+    /// Borrow the compiled code as a slice.
+    pub fn as_slice(&self) -> &[u8] {
+        &self.page
+    }
+}
+
+impl From<Mmap> for CompiledCode {
+    fn from(page: Mmap) -> Self {
+        CompiledCode { page }
+    }
+}
 
 /// Run a function on a host
 pub struct FunctionRunner {
@@ -31,29 +57,17 @@ impl FunctionRunner {
         Self::with_host_isa(function, flags)
     }
 
-    /// Compile and execute a single function, expecting a boolean to be returned; a 'true' value is
-    /// interpreted as a successful test execution and mapped to Ok whereas a 'false' value is
-    /// interpreted as a failed test and mapped to Err.
-    pub fn run(&self) -> Result<(), String> {
-        let func = self.function.clone();
-        if !(func.signature.params.is_empty()
-            && func.signature.returns.len() == 1
-            && func.signature.returns.first().unwrap().value_type.is_bool())
-        {
-            return Err(String::from(
-                "Functions must have a signature like: () -> boolean",
-            ));
-        }
-
-        if func.signature.call_conv != self.isa.default_call_conv() {
+    /// Compile the given function to machine code in the TargetIsa.
+    /// TODO improve errors; replace String with thiserror?
+    pub fn compile(&self) -> Result<CompiledCode, String> {
+        if self.function.signature.call_conv != self.isa.default_call_conv() {
             return Err(String::from(
                 "Functions only run on the host's default calling convention; remove the specified calling convention in the function signature to use the host's default.",
             ));
         }
 
         // set up the context
-        let mut context = Context::new();
-        context.func = func;
+        let mut context = Context::for_function(self.function.clone()); // TODO avoid clone
 
         // compile and encode the result to machine code
         let relocs = &mut NullRelocSink {};
@@ -75,14 +89,54 @@ impl FunctionRunner {
             );
         };
 
-        let code_page = code_page.make_exec().map_err(|e| e.to_string())?;
+        let compiled_code = CompiledCode::from(code_page.make_exec().map_err(|e| e.to_string())?);
+        Ok(compiled_code)
+    }
+
+    /// Execute compiled code with the given arguments. TODO change &[u8] to CompiledCode?
+    /// TODO better errors
+    pub fn execute(code: &[u8], arguments: &[Value]) -> Result<Value, String> {
+        match arguments.len() {
+            0 => FunctionRunner::execute0(code),
+            1 => FunctionRunner::execute1(code, arguments[0].clone()), // TODO avoid clone
+            _ => unimplemented!(),
+        }
+    }
+
+    /// Execute a compiled function with no arguments. TODO multi-value return is unlikely
+    pub fn execute0(_code: &[u8]) -> Result<Value, String> {
+        // TODO figure out how to cast code to a callable function with the right types (see wrappers! and getters! in crates/api/src/func.rs)
+        unimplemented!()
+    }
+
+    /// Execute a compiled function with a single argument. TODO multi-value return is unlikely
+    pub fn execute1(_code: &[u8], _a: Value) -> Result<Value, String> {
+        // TODO figure out how to cast code to a callable function with the right types (see wrappers! and getters! in crates/api/src/func.rs)
+        unimplemented!()
+    }
+
+    /// Compile and execute a single function, expecting a boolean to be returned; a 'true' value is
+    /// interpreted as a successful test execution and mapped to Ok whereas a 'false' value is
+    /// interpreted as a failed test and mapped to Err.
+    pub fn run(&self) -> Result<(), String> {
+        let func = self.function.clone();
+        if !(func.signature.params.is_empty()
+            && func.signature.returns.len() == 1
+            && func.signature.returns.first().unwrap().value_type.is_bool())
+        {
+            return Err(String::from(
+                "Functions must have a signature like: () -> boolean",
+            ));
+        }
+
+        let code_page = self.compile()?;
         let callable_fn: fn() -> bool = unsafe { mem::transmute(code_page.as_ptr()) };
 
         // execute
         if callable_fn() {
             Ok(())
         } else {
-            Err(format!("Failed: {}", context.func.name.to_string()))
+            Err(format!("Failed: {}", func.name.to_string()))
         }
     }
 }
